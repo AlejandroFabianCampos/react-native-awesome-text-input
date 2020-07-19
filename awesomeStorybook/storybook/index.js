cb34527e580fcf7a13de4895c9e7f136c25585d4
Alejandro Campos <camposalejandrofabian@gmail.com>
2020-07-19 01:05:35 -0300

    Removed useless files from vcs, added formatter config and formatted files

--- awesomeStorybook/storybook/index.js
+++ awesomeStorybook/storybook/index.js
@@ -1,11 +1,11 @@
-import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
+import { AppRegistry } from "react-native";
+import { getStorybookUI, configure } from "@storybook/react-native";
 
-import './rn-addons';
+import "./rn-addons";
 
 // import stories
 configure(() => {
-  require('./stories');
+	require("./stories");
 }, module);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
@@ -14,6 +14,6 @@ const StorybookUIRoot = getStorybookUI({ asyncStorage: null });
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+AppRegistry.registerComponent("%APP_NAME%", () => StorybookUIRoot);
 
 export default StorybookUIRoot;
